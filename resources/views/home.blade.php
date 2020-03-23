15bf79a8b87f1a42c8da944d6873bdd0f93f86e4
Izzat <madaminov.izzat.rustambayevich@gmail.com>
2020-03-23 10:58:43 +0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://pbs.twimg.com/profile_images/843990035431010305/KSyS7Xtu_400x400.jpg" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>blogGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>121</strong> posts</div>
+                <div class="pr-4"><strong>1.2k</strong> followers</div>
+                <div class="pr-4"><strong>46</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
