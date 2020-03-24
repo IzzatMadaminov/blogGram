f266daea85efb63ef466cb41bce35ae71addd8d9
Izzat <madaminov.izzat.rustambayevich@gmail.com>
2020-03-25 00:28:31 +0500

    Maded add new  post section

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,7 +10,8 @@ class ProfilesController extends Controller
     public function index($user)
     {
         $user = User::findorFail($user);
-        return view('home', [
+
+        return view('profiles.index', [
             'user' => $user,
         ]);
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,7 +8,10 @@
             <img src="https://pbs.twimg.com/profile_images/843990035431010305/KSyS7Xtu_400x400.jpg" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user -> username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user -> username }}</h1>
+                <a href="/p/create">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-4"><strong>121</strong> posts</div>
                 <div class="pr-4"><strong>1.2k</strong> followers</div>
@@ -25,15 +28,12 @@
     </div> -->
     
     <div class="row pt-4">
-        <div class="col-4">
-            <img src="/img/20190227_103522.jpg" style="transform:rotate(90deg);" class="w-100 h-100">
-        </div>
-        <div class="col-4">
-            <img src="/img/20190227_103522.jpg" style="transform:rotate(90deg);" class="w-100 h-100">
-        </div>
-        <div class="col-4">
-            <img src="/img/20190227_103522.jpg" style="transform:rotate(90deg);" class="w-100 h-100">
-        </div>
+        @foreach($user->posts as $post)
+            <div class="col-4">
+                <img src="/storage/{{$post->image}}" class="w-100">
+            </div>
+        @endforeach
+        
     </div>
 </div>
 @endsection
